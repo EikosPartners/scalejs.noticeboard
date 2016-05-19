c71d66f980b608b826895fd54fad69db6d0a6bd4
egucciar <egucciar@gmail.com>
2016-05-19 10:02:33 -0400

    Yo generate template for scalejs extension

---
+++ test/scalejs.noticeboard.test.js
@@ -0,0 +1,19 @@
+define([
+    'scalejs!core', 'scalejs!application'
+], function(
+    core
+) {
+    var noticeboard = core.noticeboard;
+
+    // For deeper testing, log to console
+    console.log('core.noticeboard: ', noticeboard);
+
+    describe('core.noticeboard', function() {
+
+        it('is defined', function() {
+            expect(noticeboard).toBeDefined();
+        });
+
+    });
+});
+
