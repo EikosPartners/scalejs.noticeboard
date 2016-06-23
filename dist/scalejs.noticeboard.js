93ede45050607f2bb256e033639da04cbe6527e0
Dan Tanzer <dan.tanzer@eikospartners.com>
2016-06-23 15:55:53 -0400

    covert to es6

--- dist/scalejs.noticeboard.js
+++ dist/scalejs.noticeboard.js
@@ -1,79 +1,98 @@
-define('scalejs.noticeboard',[
-    'scalejs.core',
-    'knockout'
-], function (
-    core,
-    ko
-) {
-    'use strict';
-    
-    var observable = ko.observable,
-        computed = ko.computed;
+'use strict';
 
-    function noticeboard() {
-        var dictionary = observable({}),
-            subs = {};
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.createNewNoticeboard = exports.dictionary = exports.remove = exports.subscribe = exports.set = exports.get = exports.getValue = exports.setValue = undefined;
 
-        // will set the value on an existing observable
+var _knockout = require('knockout');
 
-        function setValue(key, value) {
-            if (dictionary()[key]) {
-                dictionary()[key](value);
-            } else {
-                dictionary()[key] = observable(value);
-                dictionary.valueHasMutated();
-            }
+var _knockout2 = _interopRequireDefault(_knockout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var observable = _knockout2.default.observable,
+    computed = _knockout2.default.computed,
+    global = noticeboard();
+
+function noticeboard() {
+    var dictionary = observable({}),
+        subs = {};
+
+    // will set the value on an existing observable
+
+    function setValue(key, value) {
+        if (dictionary()[key]) {
+            dictionary()[key](value);
+        } else {
+            dictionary()[key] = observable(value);
+            dictionary.valueHasMutated();
         }
-
-        function getValue(key) {
-            var item = dictionary()[key];
-            if (item) {
-                return item();
-            }
-        }
-
-        function subscribe(key, callback) {
-            var sub = computed(function () {
-                return getValue(key);
-            });
-            sub.subscribe(function (newValue) {
-                callback(newValue);
-            });
-            callback(sub()); // When initially called
-            subs[key] = subs[key] || [];
-            subs[key].push(sub);
-            return sub;
-        }
-
-        function remove(key) {
-            if (dictionary()[key]) {
-                if (subs[key]) {
-                    subs[key].forEach(function (sub) {
-                        sub.dispose();
-                    })
-                }
-                delete dictionary()[key];
-                dictionary.valueHasMutated();
-            }
-        }
-
-        return {
-            setValue: setValue,
-            getValue: getValue,
-            get: getValue,
-            set: setValue,
-            subscribe: subscribe,
-            remove: remove,
-            dictionary: dictionary
-        };
     }
 
-    core.registerExtension({
-        noticeboard: {
-            createNewNoticeboard: noticeboard,
-            global: noticeboard()
+    function getValue(key) {
+        var item = dictionary()[key];
+        if (item) {
+            return item();
         }
-    });
+    }
+
+    function subscribe(key, callback) {
+        var sub = computed(function () {
+            return getValue(key);
+        });
+        sub.subscribe(function (newValue) {
+            callback(newValue);
+        });
+        callback(sub()); // When initially called
+        subs[key] = subs[key] || [];
+        subs[key].push(sub);
+        return sub;
+    }
+
+    function remove(key) {
+        if (dictionary()[key]) {
+            if (subs[key]) {
+                subs[key].forEach(function (sub) {
+                    sub.dispose();
+                });
+            }
+            delete dictionary()[key];
+            dictionary.valueHasMutated();
+        }
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue,
+        get: getValue,
+        set: setValue,
+        subscribe: subscribe,
+        remove: remove,
+        dictionary: dictionary
+    };
+}
+
+core.registerExtension({
+    noticeboard: {
+        createNewNoticeboard: noticeboard,
+        global: noticeboard()
+    }
 });
 
-
+var setValue = global.setValue;
+var getValue = global.getValue;
+var get = global.get;
+var set = global.set;
+var subscribe = global.subscribe;
+var remove = global.remove;
+var dictionary = global.dictionary;
+exports.setValue = setValue;
+exports.getValue = getValue;
+exports.get = get;
+exports.set = set;
+exports.subscribe = subscribe;
+exports.remove = remove;
+exports.dictionary = dictionary;
+exports.createNewNoticeboard = noticeboard;
+//# sourceMappingURL=scalejs.noticeboard.js.map
--- src/scalejs.noticeboard.js
+++ src/scalejs.noticeboard.js
@@ -1,78 +1,95 @@
-define([
-    'scalejs.core',
-    'knockout'
-], function (
-    core,
-    ko
-) {
-    'use strict';
-    
-    var observable = ko.observable,
-        computed = ko.computed;
+import ko from 'knockout';
 
-    function noticeboard() {
-        var dictionary = observable({}),
-            subs = {};
+var observable = ko.observable,
+    computed = ko.computed,
+    global = noticeboard();
 
-        // will set the value on an existing observable
+function noticeboard() {
+    var dictionary = observable({}),
+        subs = {};
 
-        function setValue(key, value) {
-            if (dictionary()[key]) {
-                dictionary()[key](value);
-            } else {
-                dictionary()[key] = observable(value);
-                dictionary.valueHasMutated();
-            }
+    // will set the value on an existing observable
+
+    function setValue(key, value) {
+        if (dictionary()[key]) {
+            dictionary()[key](value);
+        } else {
+            dictionary()[key] = observable(value);
+            dictionary.valueHasMutated();
         }
-
-        function getValue(key) {
-            var item = dictionary()[key];
-            if (item) {
-                return item();
-            }
-        }
-
-        function subscribe(key, callback) {
-            var sub = computed(function () {
-                return getValue(key);
-            });
-            sub.subscribe(function (newValue) {
-                callback(newValue);
-            });
-            callback(sub()); // When initially called
-            subs[key] = subs[key] || [];
-            subs[key].push(sub);
-            return sub;
-        }
-
-        function remove(key) {
-            if (dictionary()[key]) {
-                if (subs[key]) {
-                    subs[key].forEach(function (sub) {
-                        sub.dispose();
-                    })
-                }
-                delete dictionary()[key];
-                dictionary.valueHasMutated();
-            }
-        }
-
-        return {
-            setValue: setValue,
-            getValue: getValue,
-            get: getValue,
-            set: setValue,
-            subscribe: subscribe,
-            remove: remove,
-            dictionary: dictionary
-        };
     }
 
-    core.registerExtension({
-        noticeboard: {
-            createNewNoticeboard: noticeboard,
-            global: noticeboard()
+    function getValue(key) {
+        var item = dictionary()[key];
+        if (item) {
+            return item();
         }
-    });
+    }
+
+    function subscribe(key, callback) {
+        var sub = computed(function () {
+            return getValue(key);
+        });
+        sub.subscribe(function (newValue) {
+            callback(newValue);
+        });
+        callback(sub()); // When initially called
+        subs[key] = subs[key] || [];
+        subs[key].push(sub);
+        return sub;
+    }
+
+    function remove(key) {
+        if (dictionary()[key]) {
+            if (subs[key]) {
+                subs[key].forEach(function (sub) {
+                    sub.dispose();
+                })
+            }
+            delete dictionary()[key];
+            dictionary.valueHasMutated();
+        }
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue,
+        get: getValue,
+        set: setValue,
+        subscribe: subscribe,
+        remove: remove,
+        dictionary: dictionary
+    };
+}
+
+core.registerExtension({
+    noticeboard: {
+        createNewNoticeboard: noticeboard,
+        global: noticeboard()
+    }
 });
+
+
+let {
+    setValue,
+    getValue,
+    get,
+    set,
+    subscribe,
+    remove,
+    dictionary
+} = global;
+
+export {
+    setValue,
+    getValue,
+    get,
+    set,
+    subscribe,
+    remove,
+    dictionary,
+    noticeboard as createNewNoticeboard
+}
+
+
 
